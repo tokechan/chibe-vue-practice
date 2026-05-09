94b3e95797547075c477cb6382d9408f4c01846f
tokec <fleatoke@gmail.com>
2026-05-09 15:41:34 +0900

    done: 010

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,1 +1,9 @@
-import "chibivue";
+import { createApp } from "chibivue";
+
+const app = createApp({
+  render() {
+    return "Hello World";
+  },
+});
+
+app.mount("#app");
--- packages/index.ts
+++ packages/index.ts
@@ -1,1 +1,20 @@
 console.log("chibivue is working");
+
+export type Options = {
+  render: () => string;
+};
+
+export type App = {
+  mount: (selector: string) => void;
+};
+
+export const createApp = (options: Options): App => {
+  return {
+    mount: (selector) => {
+      const root = document.querySelector(selector);
+      if (root) {
+        root.innerHTML = options.render();
+      }
+    },
+  };
+};
